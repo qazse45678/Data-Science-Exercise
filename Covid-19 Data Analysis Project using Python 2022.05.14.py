ff454a61b925f1ac856c5868a5ae557511f06045
Mary Lin <63503783+qazse45678@users.noreply.github.com>
2022-05-14 15:17:02 +0800

    Update and rename Covid-19 Data Analysis Project using Python 2022.05.14 to Covid-19 Data Analysis Project using Python 2022.05.14.py

--- Covid-19 Data Analysis Project using Python 2022.05.14.py
+++ Covid-19 Data Analysis Project using Python 2022.05.14.py
@@ -7,3 +7,7 @@ data = pd.read_csv('/Users/marylin/文件/程式語言/Python/DS EDA Project 202
 
 data.head()
 data.columns
+data.tail()
+data.describe()
+data.columns
+sns.relplot(x = "total_cases", y = "recovered", data = data)
