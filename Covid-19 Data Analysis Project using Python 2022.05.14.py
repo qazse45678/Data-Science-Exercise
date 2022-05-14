01f5d87222f2cccbd3859544b5330d6588a60784
Mary Lin <63503783+qazse45678@users.noreply.github.com>
2022-05-14 15:23:26 +0800

    Update Covid-19 Data Analysis Project using Python 2022.05.14.py

--- Covid-19 Data Analysis Project using Python 2022.05.14.py
+++ Covid-19 Data Analysis Project using Python 2022.05.14.py
@@ -11,3 +11,18 @@ data.tail()
 data.describe()
 data.columns
 sns.relplot(x = "total_cases", y = "recovered", data = data)
+sns.relplot(x = "total_cases", y = "deaths", data = data)
+sns.relplot(x = "total_cases", y = "total_confirmed_cases", data = data)
+sns.relplot(x = "total_cases", y = "hospitalized_with_symptoms", data = data)
+sns.relplot(x = "total_cases", y = "hospitalized_with_symptoms", hue = "recovered", data = data)
+sns.pairplot(data)
+data.columns
+sns.relplot(x = "total_cases", y = "home_quarantine", kind = "line", data = data)
+sns.relplot(x = "recovered", y = "home_quarantine", kind = "line", data = data)
+data.columns
+sns.catplot(x = "total_cases", y = "recovered", data = data)
+
+#Three main conclusions in the project:
+#1. More people are in recovery with the total number of cases if more people are under quarantined
+#2. With more total cases, the number of hospitalized people is actually flattening
+#3. The number of death is increasing
