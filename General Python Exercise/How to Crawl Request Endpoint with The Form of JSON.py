f94a7fe4a0656400a772ba21a41e1a36de1156b0
Mary Lin <63503783+qazse45678@users.noreply.github.com>
2022-11-18 11:18:35 +0800

    Create How to Crawl Request Endpoint with The Form of JSON.py

---
+++ General Python Exercise/How to Crawl Request Endpoint with The Form of JSON.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+#url 要放 endpoint url，不是網頁 url
+url = 'https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2020-21&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight='
+custom_headers = {
+    'Host': 'stats.nba.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'x-nba-stats-origin': 'stats',
+    'x-nba-stats-token': 'true',
+    'Connection': 'keep-alive',
+    'Referer': 'https://stats.nba.com/',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
+
+resp = requests.get(url, headers = custom_headers)
+datas = json.loads(resp.text)
+players = datas['resultSets'][0]['rowSet']
+
+for i in players:
+    name = i[1]
+    print(name)
